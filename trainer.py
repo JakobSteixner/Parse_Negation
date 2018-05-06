7b48a92792f891de884b3f310c18443dc5561de8
JMSteixner <jok83@gmx.at>
2018-05-06 23:33:23 +0200

    json input for training examples

--- trainer.py
+++ trainer.py
@@ -1,7 +1,7 @@
 #/usr/bin/python3.6
 # -*- coding: utf8 -*-
 
-import spacy, plac
+import spacy, plac, json
 import random
 from pathlib import Path
 from spacy import displacy
@@ -11,89 +11,23 @@ nlp.entity.add_label("NEGATION")
 nlp.entity.add_label("NEGID")
 
 
-TRAIN_DATA = [
-    
-    #('Ich sage das nicht nur, weil ich ihn nicht mag.', {
-    #    'entities': [(13, 18, "NEGATION"), (19, 22, 'NEGID'), (37, 42, "NEGATION")]
-    #}),
-    #('Er bestreitet, jemals dort gewesen zu sein', {
-    #    'entities': [(3, 13, 'NEGID')]
-    #}),
-    #('Ich zweifle, dass er heute noch anruft.', {
-    #    'entities': [(4, 11, 'NEGID')]
-    #}),
-    ('Warum kommst du heute nicht?', {
-        'entities': [(22, 27, 'NEGATION')]
-    }),
-    ('Ich habe keine Lust auf Apfelmus', {
-        'entities': [(9, 14, "NEGATION")]
-    }),
-    ('Peter will lieber nicht heute kommen', {
-        "entities": [(0, 5, "PERS"), (18, 23, "NEGATION")]
-        }
-    ),
-    (u'In China leben keine Kängurus.', {
-        "entities": [(3, 8, "GPE"), (15, 20, "NEGATION")]
-        }
-     ),
-    
-    (u'Ich bin mir keiner Schuld bewusst.', {
-        "entities": [(12, 18, "NEGATION")]
-        }
-     ),
-    
-    (u'Schild habe ich keines gesehen.', {
-        "entities": [(16, 22, "NEGATION")]
-        }
-     ),
-    
-    (u'Donald Trump bezweifelt, dass Nordkorea zur Abrüstung bereit ist.', {
-        "entities": [(13, 23,"NEGATION")]
-        }
-    ),
-     
-    
-    (u'Es bleibt zu bezweifeln, dass die Friedensverhandlungen zu einer Lösung führen werden.', {
-        "entities": [(13, 23,"NEGATION")]
-        }
-     ),
-    
-    
-    (u'Ich bezweifle, dass du mir das sagen kannst.', {
-        "entities": [(4, 13,"NEGATION")]
-        }
-     ),
-    
-    (u'Immer noch bestreitet er, dass er etwas falsch gemacht hat.', {
-        "entities": [(11, 21, "NEGATION")]
-        }
-     ),
-    
-    (u'Niemand wird dir das glauben.', {
-        "entities": [(0, 7, "NEGATION")]
-        }
-     ),
-    
-    (u'Da bin ich ja nochmal günstig davongekommen', {
-        "entities": []
-        }
-    ),
-    
-    (u'Ich schaffe es doch erst morgen.', {
-        "entities": []
-        }
-     )
-    
-]
+TRAIN_DATA = json.load(open("TRAINING_DATA.json"))
+print (TRAIN_DATA)
+for entry in TRAIN_DATA:
+    print (entry)
+    print (len(entry))
 
 @plac.annotations(
     model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
     output_dir=("Optional output directory", "option", "o", Path),
     n_iter=("Number of training iterations", "option", "n", int),
     threshold_success=("losses threshold to declare success", "option", "t", float),
-    successive_successes=("Number of successful runs before terminating", "option", "s", int))
+    successive_successes=("Number of successful runs before terminating", "option", "s", int),
+    input_data=("File name/path for training data", "option", "i", str),
+    param = ("Parameter to be trained", "option", "p", str)
+    )
 class Trainer:
-    def __init__(self, model=None, output_dir=None, n_iter=50, threshold_success=None, successive_successes=3):
+    def __init__(self, model=None, output_dir=None, n_iter=50, threshold_success=None, successive_successes=3, input_data = "TRAINING_DATA.json", param='ner'):
         """Load the model, set up the pipeline and train the entity recognizer."""
         if model is not None:
             self.nlp = nlp = spacy.l
@@ -106,19 +40,22 @@ class Trainer:
         self.n_iter = n_iter
         self.threshold_success = threshold_success
         self.successive_successes = successive_successes
-        self.train()
-    def train(self):
+        self.TRAIN_DATA = json.load(open(input_data))
+    
+        self.train(param)
+    def train(self, param):
         # create the built-in pipeline components and add them to the pipeline
         # nlp.create_pipe works for built-ins that are registered with spaCy
-        if 'ner' not in nlp.pipe_names:
-            ner = self.nlp.create_pipe('ner')
+        if param not in nlp.pipe_names:
+            ner = self.nlp.create_pipe(param)
             nlp.add_pipe(ner, last=True)
         # otherwise, get it so we can add labels
         else:
-            ner = self.nlp.get_pipe('ner')
+            ner = self.nlp.get_pipe(param)
     
         # add labels
-        for _, annotations in TRAIN_DATA:
+        print (self.TRAIN_DATA)
+        for _, annotations in self.TRAIN_DATA:
             for ent in annotations.get('entities'):
                 ner.add_label(ent[2])
         
@@ -132,15 +69,15 @@ class Trainer:
             return 0
     
         # get names of other pipes to disable them during training
-        other_pipes = [pipe for pipe in self.nlp.pipe_names if pipe != 'ner']
+        other_pipes = [pipe for pipe in self.nlp.pipe_names if pipe != param]
         with self.nlp.disable_pipes(*other_pipes):  # only train NER
             optimizer = nlp.begin_training()
             successes = 0
             runs = 0
             while continue_condition(runs,successes):
-                random.shuffle(TRAIN_DATA)
+                random.shuffle(self.TRAIN_DATA)
                 losses = {}
-                for text, annotations in TRAIN_DATA:
+                for text, annotations in self.TRAIN_DATA:
                     self.nlp.update(
                         [text],  # batch of texts
                         [annotations],  # batch of annotations
@@ -155,7 +92,7 @@ class Trainer:
                 runs += 1
     def test(self):
         # test the trained model
-        for text, _ in TRAIN_DATA:
+        for text, _ in self.TRAIN_DATA:
             doc = nlp(text)
             print('Entities', [(ent.text, ent.label_) for ent in doc.ents])
             print('Tokens', [(t.text, t.ent_type_, t.ent_iob) for t in doc])
